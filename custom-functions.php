e4a699adebb47db5a654524b302fc0ce7c9ba093
Pippin Williamson <pippin@pippin.pagelydev.com>
2016-09-20 14:50:09 +0000

    Adding files

---
+++ custom-functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Custom Functions
+ * Plugin URI: http://applyfilters.fm
+ * Description: Custom functionality for Apply Filters website
+ * Author: Pippin Williamson and Brad Touesnard
+ * Author URI: http://applyfilters.fm
+ * Version: 100
+ */
+
